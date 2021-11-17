9fb54a2643923af7a525f89bc7c737934e11a5fc
Duarte Molina <godl@gft.com>
2021-11-17 20:29:49 -0300

    initial commit

---
+++ src/main/java/com/gft/plannercrud/PlannerCrudApplication.java
@@ -0,0 +1,13 @@
+package com.gft.plannercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gft/plannercrud/PlannerCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.plannercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
