8f066071235b8c82e75e34656a131aedcfbed773
mahiro7 <guilhermemolina00@gmail.com>
2021-11-23 01:46:01 -0300

    CourseRepository updated

--- src/main/java/com/gft/plannercrud/repositories/CourseRepository.java
+++ src/main/java/com/gft/plannercrud/repositories/CourseRepository.java
@@ -2,6 +2,10 @@ package com.gft.plannercrud.repositories;
 
 import com.gft.plannercrud.entities.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    @Query(value = "SELECT c FROM Course c WHERE c.code = :code")
+    Course findCourseByCode(@Param("code") String code);
 }
